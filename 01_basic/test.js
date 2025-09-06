d394c40fa69ffcb52e1a457b9cf133b0a4f6417f
Divyu <skydkarya@gmail.com>
2025-09-06 11:04:00 +0000

    test.js

---
+++ 01_basic/test.js
@@ -0,0 +1,16 @@
+const accountID = 12534
+let accountMail = "sky@gmail.com"
+var accountPassword = "74658"
+let AccountCity = "delhi"
+let accountState
+
+//console.log(accountID);
+
+
+accountMail = "dk@gmaail.com"
+accountPassword = "54645"
+AccountCity = "new delhi"
+
+//console.log([accountID, accountMail, accountPassword, AccountCity]);
+console.table([accountID, accountMail, accountPassword, AccountCity, accountState])
+
